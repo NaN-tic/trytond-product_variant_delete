a36124a045c5c0472bf33b94c4db1b52940a7d93
jmartin <jmartin@zikzakmedia.com>
2014-11-07 14:33:32 +0100

    Cleanup tests

--- tests/test_product_variant_delete.py
+++ tests/test_product_variant_delete.py
@@ -1,32 +1,20 @@
 #!/usr/bin/env python
-#This file is part product_product module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains
-#the full copyright notices and license terms.
-
-import sys
-import os
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
-
+# This file is part product_product module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import test_view, test_depends
 
 
 class ProductVariantDeleteCase(unittest.TestCase):
-    '''
-    Test Product Variant Delete module.
-    '''
+    'Test Product Variant Delete module'
 
     def setUp(self):
         trytond.tests.test_tryton.install_module('product_variant_delete')
 
     def test0006depends(self):
-        '''
-        Test depends.
-        '''
+        'Test depends'
         test_depends()
 
 
