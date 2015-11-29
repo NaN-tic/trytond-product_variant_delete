9232a419d539a2d3b15efdea9256fc2b03871c98
resteve <resteve@zikzakmedia.com>
2015-11-29 23:02:37 +0100

    Tests

--- tests/test_product_variant_delete.py
+++ tests/test_product_variant_delete.py
@@ -1,21 +1,14 @@
-#!/usr/bin/env python
-# This file is part product_product module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the product_variant_delete module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class ProductVariantDeleteCase(unittest.TestCase):
+class ProductVariantDeleteCase(ModuleTestCase):
     'Test Product Variant Delete module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('product_variant_delete')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'product_variant_delete'
 
 
 def suite():
@@ -23,6 +16,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         ProductVariantDeleteCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
