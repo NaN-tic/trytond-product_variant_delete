cd6ae546018e2bddbaaf51dc166dc9689d0ebc71
Raimon Esteve <raimon@nan-tic.com>
2018-09-15 18:11:53 +0200

    Migrate to py3

--- __init__.py
+++ __init__.py
@@ -2,9 +2,9 @@
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 from trytond.pool import Pool
-from .product import *
+from . import product
 
 def register():
     Pool.register(
-        Product,
+        product.Product,
         module='product_variant_delete', type_='model')
