5b1e77c379d2a2e8c1fa8995e036b7be57a9612b
raimonesteve <raimonesteve@gmail.com>
2020-07-05 11:09:07 +0200

    Upgrade to 5.6

--- product.py
+++ product.py
@@ -2,6 +2,8 @@
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 from trytond.pool import PoolMeta
+from trytond.i18n import gettext
+from trytond.exceptions import UserError
 
 __all__ = ['Product']
 
@@ -10,16 +12,11 @@ class Product(metaclass=PoolMeta):
     __name__ = 'product.product'
 
     @classmethod
-    def __setup__(cls):
-        super(Product, cls).__setup__()
-        cls._error_messages.update({
-                'delete_last_variant': ('Not allow to delete last variant.'),
-                })
-
-    @classmethod
     def delete(cls, products):
         for product in products:
             prods = product.template.products
             if len(prods) <= 1:
-                cls.raise_user_error('delete_last_variant', (product.rec_name,))
+                raise UserError(
+                    gettext('product_variant_delete.msg_delete_last_variant',
+                    product=product.rec_name))
         super(Product, cls).delete(products)
